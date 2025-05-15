24cc024fdfe2820e027930dc345b61c02c85a805
Arthur Santos <a4hur78@gmail.com>
2025-05-15 16:37:11 -0300

    Add files via upload

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,15 @@
+
+import { StyleSheet, Text } from 'react-native';
+
+
+export default function TabTwoScreen() {
+  return (
+   <>
+    <Text>Segunda Pagina</Text>
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <>
+      <Text>INDEX principal das tabs do segundo layout</Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
